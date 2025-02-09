5d3fe4316ef4b25148688b0b7d0e17d3c5a2ef6d
Alisa Borkowska <alisa.borkowska@gmail.com>
2025-02-10 00:53:07 +0100

    best score functionality in storage and DOM

--- math-sprint-game/script.js
+++ math-sprint-game/script.js
@@ -22,6 +22,7 @@ const playAgainBtn = document.querySelector('.play-again')
 let questionAmount = 0
 let equationsArray = []
 let playerGuessArray = []
+let bestScoreArray = []
 
 // Game Page
 let firstNumber = 0
@@ -35,11 +36,102 @@ let timePlayed = 0
 let baseTime = 0
 let penaltyTime = 0
 let finalTime = 0
-let finalTimeDisplay = '0.0s'
+let finalTimeDisplay = '0.0'
 
 // Scroll
 let valueY = 0
 
+// Refresh Splash Page Best Scores
+function bestScoresToDOM() {
+  bestScores.forEach((bestScore, index) => {
+    const bestScoreEl = bestScore
+    bestScoreEl.textContent = `${bestScoreArray[index].bestScore}`
+  })
+}
+
+// Check Local Storage for Best Scores, set bestScoreArray
+function getSavedBestScores() {
+  if (localStorage.getItem('bestScores')) {
+    bestScoreArray = JSON.parse(localStorage.bestScores)
+  } else {
+    bestScoreArray = [
+      {
+        questions: 10,
+        bestScore: finalTimeDisplay,
+      },
+      {
+        questions: 25,
+        bestScore: finalTimeDisplay,
+      },
+      {
+        questions: 50,
+        bestScore: finalTimeDisplay,
+      },
+      {
+        questions: 99,
+        bestScore: finalTimeDisplay,
+      },
+    ]
+    localStorage.setItem('bestScores', JSON.stringify(bestScoreArray))
+  }
+  bestScoresToDOM()
+}
+
+// Update Best Score Array
+function updateBestScore() {
+  bestScoreArray.forEach((score, index) => {
+    // Select correct Best Score to update
+    if (questionAmount == score.questions) {
+      console.log(questionAmount, score.questions)
+      // Return Best Score as number with one decimal
+      const savedBestScore = Number(bestScoreArray[index].bestScore)
+      console.log(savedBestScore)
+      // Update if the new final score is less or replacing zero
+      if (savedBestScore === 0 || savedBestScore > finalTime) {
+        bestScoreArray[index].bestScore = finalTimeDisplay
+      }
+    }
+  })
+  // Update Splash Page
+  bestScoresToDOM()
+  // Save to Local Storage
+  localStorage.setItem('bestScores', JSON.stringify(bestScoreArray))
+}
+
+// Reset Game
+function playAgain() {
+  gamePage.addEventListener('click', startTimer)
+  scorePage.hidden = true
+  splashPage.hidden = false
+  equationsArray = []
+  playerGuessArray = []
+  valueY = 0
+  playAgainBtn.hidden = true
+}
+
+// Show Score Page
+function showScorePage() {
+  // Show Play Again button after 1 second
+  setTimeout(() => {
+    playAgainBtn.hidden = false
+  }, 1000)
+  gamePage.hidden = true
+  scorePage.hidden = false
+}
+
+// Format & Display Time in DOM
+function scoresToDOM() {
+  finalTimeDisplay = finalTime.toFixed(1)
+  baseTime = timePlayed.toFixed(1)
+  penaltyTime = penaltyTime.toFixed(1)
+  baseTimeEl.textContent = `Base Time: ${baseTime}s`
+  penaltyTimeEl.textContent = `Penalty Time: +${penaltyTime}s`
+  finalTimeEl.textContent = `${finalTimeDisplay}s`
+  updateBestScore()
+  // Scroll to Top, go to Score Page
+  itemContainer.scrollTo({ top: 0, behavior: 'instant' })
+  showScorePage()
+}
 // Stop Timer, Process Results, go to Score Page
 function checkTime() {
   console.log(timePlayed)
@@ -64,6 +156,7 @@ function checkTime() {
       'final time',
       finalTime
     )
+    scoresToDOM()
   }
 }
 
@@ -195,7 +288,7 @@ function showCountdown() {
   splashPage.hidden = true
   countdownStart()
   populateGamePage()
-  setTimeout(showGamePage, 400)
+  setTimeout(showGamePage, 4000)
 }
 
 // Get the value from selected radio button
@@ -234,3 +327,6 @@ startForm.addEventListener('click', () => {
 // Event Listeners
 startForm.addEventListener('submit', selectQuestionAmount)
 gamePage.addEventListener('click', startTimer)
+
+// On Load
+getSavedBestScores()
