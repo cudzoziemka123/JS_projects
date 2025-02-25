b15e08886e732e2f2a5b4eb8480cb48923fab599
Alisa <alisa.borkowska@gmail.com>
2025-02-25 14:22:30 +0100

    fixes

--- joke-teller/script.js
+++ joke-teller/script.js
@@ -1,95 +1,95 @@
-const button = document.getElementById('button')
-const audioElement = document.getElementById('audio')
+const button = document.getElementById("button");
+const audioElement = document.getElementById("audio");
 
 // Disable/Enable Button
 function toggleButton() {
-  button.disabled = !button.disabled
+  button.disabled = !button.disabled;
 }
 
 function tellMe(joke) {
   VoiceRSS.speech({
-    key: '49eebf525cd644e7b588cbb1b7203baa',
+    key: "49eebf525cd644e7b588cbb1b7203baa",
     src: `${joke}`,
-    hl: 'en-us',
-    v: 'Linda',
+    hl: "en-us",
+    v: "Linda",
     r: 0,
-    c: 'mp3',
-    f: '44khz_16bit_stereo',
+    c: "mp3",
+    f: "44khz_16bit_stereo",
     ssml: false,
-  })
+  });
 }
 
 // Get Jokes from Joke API
 async function getJokes() {
-  let joke = ''
+  let joke = "";
   const apiUrl =
-    'https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit'
+    "https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit";
 
   try {
-    const response = await fetch(apiUrl)
-    const data = await response.json()
+    const response = await fetch(apiUrl);
+    const data = await response.json();
     if (data.setup) {
-      joke = `${data.setup}... ${data.delivery}`
+      joke = `${data.setup}... ${data.delivery}`;
     } else {
-      joke = data.joke
+      joke = data.joke;
     }
     // Text-to-Speech
-    tellMe(joke)
+    tellMe(joke);
 
     // Disable Button
-    toggleButton()
+    toggleButton();
   } catch (error) {
     // Catch Errors Here
-    console.log('whoops', error)
+    console.log("whoops", error);
   }
 }
-button.addEventListener('click', getJokes)
-audioElement.addEventListener('ended', toggleButton)
+button.addEventListener("click", getJokes);
+audioElement.addEventListener("ended", toggleButton);
 
-const button = document.getElementById('button')
-const audioElement = document.getElementById('audio')
+// const button = document.getElementById('button')
+// const audioElement = document.getElementById('audio')
 
-// Disable/Enable Button
-function toggleButton() {
-  button.disabled = !button.disabled
-}
+// // Disable/Enable Button
+// function toggleButton() {
+//   button.disabled = !button.disabled
+// }
 
-function tellMe(joke) {
-  VoiceRSS.speech({
-    key: '49eebf525cd644e7b588cbb1b7203baa',
-    src: `${joke}`,
-    hl: 'en-us',
-    v: 'Linda',
-    r: 0,
-    c: 'mp3',
-    f: '44khz_16bit_stereo',
-    ssml: false,
-  })
-}
+// function tellMe(joke) {
+//   VoiceRSS.speech({
+//     key: '49eebf525cd644e7b588cbb1b7203baa',
+//     src: `${joke}`,
+//     hl: 'en-us',
+//     v: 'Linda',
+//     r: 0,
+//     c: 'mp3',
+//     f: '44khz_16bit_stereo',
+//     ssml: false,
+//   })
+// }
 
-// Get Jokes from Joke API
-async function getJokes() {
-  let joke = ''
-  const apiUrl =
-    'https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit'
+// // Get Jokes from Joke API
+// async function getJokes() {
+//   let joke = ''
+//   const apiUrl =
+//     'https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit'
 
-  try {
-    const response = await fetch(apiUrl)
-    const data = await response.json()
-    if (data.setup) {
-      joke = `${data.setup}... ${data.delivery}`
-    } else {
-      joke = data.joke
-    }
-    // Text-to-Speech
-    tellMe(joke)
+//   try {
+//     const response = await fetch(apiUrl)
+//     const data = await response.json()
+//     if (data.setup) {
+//       joke = `${data.setup}... ${data.delivery}`
+//     } else {
+//       joke = data.joke
+//     }
+//     // Text-to-Speech
+//     tellMe(joke)
 
-    // Disable Button
-    toggleButton()
-  } catch (error) {
-    // Catch Errors Here
-    console.log('whoops', error)
-  }
-}
-button.addEventListener('click', getJokes)
-audioElement.addEventListener('ended', toggleButton)
+//     // Disable Button
+//     toggleButton()
+//   } catch (error) {
+//     // Catch Errors Here
+//     console.log('whoops', error)
+//   }
+// }
+// button.addEventListener('click', getJokes)
+// audioElement.addEventListener('ended', toggleButton)
